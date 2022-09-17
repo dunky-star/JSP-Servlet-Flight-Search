76a739c6837e58947afb614b77b37ddcf3f87c9d
dunky-star <dukygeoffrey39@gmail.com>
2022-09-17 18:00:06 +0300

    Create Flight entity

--- flyaway-Search/src/main/java/com/dunky/flyaway/model/Flight.java
+++ flyaway-Search/src/main/java/com/dunky/flyaway/model/Flight.java
@@ -1,5 +1,27 @@
 package com.dunky.flyaway.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Flight")
+
 public class Flight {
+	
+	private int flightNumber;
+	private String from;
+	private String to;
+	private int seat;
+	private String flightTime;
+	private String flightDate;
+	private String Type;
 
 }
