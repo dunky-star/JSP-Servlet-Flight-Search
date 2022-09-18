846c8799d75cffd9692059aa79a969b0db8224f6
dunky-star <dukygeoffrey39@gmail.com>
2022-09-18 14:10:36 +0300

    Create Passengers entity

--- flyaway-Search/src/main/java/com/dunky/flyaway/entity/Passengers.java
+++ flyaway-Search/src/main/java/com/dunky/flyaway/entity/Passengers.java
@@ -17,17 +17,22 @@ public class Passengers {
 	
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "passengerId")
+    @Column(name = "passenger_id")
     private int passengerId;
 	
+	@Column(name = "first_name")
 	private String firstName;
 	
+	@Column(name = "last_name")
 	private String lastName;
 	
+	@Column(name = "age")
 	private int age;
 	
+	@Column(name = "gender")
 	private String gender;
 	
+	@Column(name = "seat_number")
 	private int seatNumber;
 	
 	@ManyToOne(cascade = CascadeType.ALL)
