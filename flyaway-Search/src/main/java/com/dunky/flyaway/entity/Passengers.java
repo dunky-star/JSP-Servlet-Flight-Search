4b1ee65e46f7712f9bc647084f9590ccabbb6e61
dunky-star <dukygeoffrey39@gmail.com>
2022-09-18 13:08:44 +0300

    Create Passengers Entity

--- flyaway-Search/src/main/java/com/dunky/flyaway/entity/Passengers.java
+++ flyaway-Search/src/main/java/com/dunky/flyaway/entity/Passengers.java
@@ -1,6 +1,23 @@
 package com.dunky.flyaway.entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+
+@Entity
+@Table(name = "Passengers")
 public class Passengers {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
 
 }
