10a17ffc86d5eb3ef3b5d21c1e4684999ce01d27
dunky-star <dukygeoffrey39@gmail.com>
2022-09-19 10:11:35 +0300

    Update Flight Data Access Object

--- flyaway-Search/src/main/java/com/dunky/flyaway/dao/FlightDao.java
+++ flyaway-Search/src/main/java/com/dunky/flyaway/dao/FlightDao.java
@@ -7,5 +7,84 @@ import com.dunky.flyaway.entity.Flight;
 import com.dunky.flyaway.util.HibernateUtil;
 
 public class FlightDao {
+	
+	public void saveFlight(Flight flight) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the Flight object
+            session.save(flight);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public void updateFlight(Flight flight) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the Flight object
+            session.update(flight);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteFlight(int flight_number) {
+
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+
+            // Delete a Flight object
+            Flight flight = session.get(Flight.class, flight_number);
+            if (flight != null) {
+                session.delete(flight);
+                System.out.println("flight is deleted");
+            }
+
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+    
+    public Flight getFlight(int flight_number) {
+
+        Transaction transaction = null;
+        Flight flight = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // get a flight object
+            flight = session.get(Flight.class, flight_number);
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return flight;
+    }
+
 
 }
