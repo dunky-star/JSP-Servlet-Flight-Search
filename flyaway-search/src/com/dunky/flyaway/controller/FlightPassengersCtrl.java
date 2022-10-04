69d951c1726e41f0e5b10e7dc795dba97fb48615
dunky-star <dukygeoffrey39@gmail.com>
2022-10-05 01:11:28 +0300

    Update FlightPassengers controller to display all flight information

--- flyaway-search/src/com/dunky/flyaway/controller/FlightPassengersCtrl.java
+++ flyaway-search/src/com/dunky/flyaway/controller/FlightPassengersCtrl.java
@@ -1,11 +1,14 @@
 package com.dunky.flyaway.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 import com.dunky.flyaway.dao.FlightDao;
 import com.dunky.flyaway.entity.Flight;
@@ -34,16 +37,57 @@ public class FlightPassengersCtrl extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			// read the "command" parameter
+			String theCommand = request.getParameter("command");
+			
+			// if the command is missing, then default to listing students
+			if (theCommand == null) {
+				theCommand = "LIST";
+			}
+			
+			// route to the appropriate method
+			switch (theCommand) {
+			
+			case "LIST":
+				listFlights(request, response);
+				break;
+								
+		     }
+		
+	      }
+	        catch (Exception exc) {
+		      throw new ServletException(exc);
+	        }
+	}
+	
+	
+    // Method to send details to Flight JSP for display of Flight information.
+	private void listFlights(HttpServletRequest request, HttpServletResponse response) 
+		throws Exception {
+
+		// get flights from db util
+		List<Flight> flights = flightDao.getAllFlight();
+		
+		// add students to the request
+		request.setAttribute("FLIGHT_LIST", flights);
+				
+		// send to JSP page (view)
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/list-flights.jsp");
+		dispatcher.forward(request, response);
 	}
 
+	
+		
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+        
+    }
+
 
 }
+
+
+
