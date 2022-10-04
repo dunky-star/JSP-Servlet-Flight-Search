1dff2c9b565bc6335bc758acdc5735a962595777
dunky-star <dukygeoffrey39@gmail.com>
2022-10-05 00:11:42 +0300

    Create list-flight JSP page

--- flyaway-search/src/com/dunky/flyaway/FlyAwayApp.java
+++ flyaway-search/src/com/dunky/flyaway/FlyAwayApp.java
@@ -1,41 +1,36 @@
-package com.dunky.flyaway;
-
-import com.dunky.flyaway.dao.PassengersDao;
-import com.dunky.flyaway.dao.FlightDao;
-import com.dunky.flyaway.entity.Flight;
-import com.dunky.flyaway.entity.Passengers;
-import com.dunky.flyaway.entity.FlightTicket;
-
-public class FlyAwayApp {
-    public static void main(String[] args) {
-
-        FlightDao flightDao = new FlightDao();
-        PassengersDao passengersDao = new PassengersDao();
-
-        Flight flight = new Flight("Kigali", "Joburg", 100, "05:00", "2023-01-01",  "Economy");
-        flightDao.saveFlight(flight);
-        
-        Flight flight1 = new Flight("EBB", "DXB", 100, "16:45", "2022-12-25",  "Business");
-        flightDao.saveFlight(flight1);
-
-        // create some passenger
-        FlightTicket ticket1 = new FlightTicket();
-        ticket1.getFlight_ticket();
-        
-        Passengers passenger1 = new Passengers("Lar", "Arma", 12, "Female", "30J", ticket1);
-        passenger1.setFlight(flight);
-        passengersDao.savePassengers(passenger1);
-
-        FlightTicket ticket2 = new FlightTicket();
-        ticket2.getFlight_ticket();
-        Passengers passenger2 = new Passengers("Ayo", "Ted", 20, "Female", "40D", ticket2);
-        passenger2.setFlight(flight1);
-        passengersDao.savePassengers(passenger2);
-        
-        FlightTicket ticket3 = new FlightTicket();
-        ticket3.getFlight_ticket();
-        Passengers passenger3 = new Passengers("Anjana", "Nice", 23, "Male", "37K", ticket3);
-        passenger3.setFlight(flight);
-        passengersDao.savePassengers(passenger3);
-    }
-}
+/*
+ * package com.dunky.flyaway;
+ * 
+ * import com.dunky.flyaway.dao.PassengersDao; import
+ * com.dunky.flyaway.dao.FlightDao; import com.dunky.flyaway.entity.Flight;
+ * import com.dunky.flyaway.entity.Passengers; import
+ * com.dunky.flyaway.entity.FlightTicket;
+ * 
+ * public class FlyAwayApp { public static void main(String[] args) {
+ * 
+ * FlightDao flightDao = new FlightDao(); PassengersDao passengersDao = new
+ * PassengersDao();
+ * 
+ * Flight flight = new Flight("Kigali", "Joburg", 100, "05:00", "2023-01-01",
+ * "Economy"); flightDao.saveFlight(flight);
+ * 
+ * Flight flight1 = new Flight("EBB", "DXB", 100, "16:45", "2022-12-25",
+ * "Business"); flightDao.saveFlight(flight1);
+ * 
+ * // create some passenger FlightTicket ticket1 = new FlightTicket();
+ * ticket1.getFlight_ticket();
+ * 
+ * Passengers passenger1 = new Passengers("Lar", "Arma", 12, "Female", "30J",
+ * ticket1); passenger1.setFlight(flight);
+ * passengersDao.savePassengers(passenger1);
+ * 
+ * FlightTicket ticket2 = new FlightTicket(); ticket2.getFlight_ticket();
+ * Passengers passenger2 = new Passengers("Ayo", "Ted", 20, "Female", "40D",
+ * ticket2); passenger2.setFlight(flight1);
+ * passengersDao.savePassengers(passenger2);
+ * 
+ * FlightTicket ticket3 = new FlightTicket(); ticket3.getFlight_ticket();
+ * Passengers passenger3 = new Passengers("Anjana", "Nice", 23, "Male", "37K",
+ * ticket3); passenger3.setFlight(flight);
+ * passengersDao.savePassengers(passenger3); } }
+ */
--- flyaway-search/src/com/dunky/flyaway/util/HibernateUtil.java
+++ flyaway-search/src/com/dunky/flyaway/util/HibernateUtil.java
@@ -1,44 +1,57 @@
 package com.dunky.flyaway.util;
 
+import java.util.Properties;
+
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
 
+import com.dunky.flyaway.entity.Flight;
+import com.dunky.flyaway.entity.Passengers;
+
+/**
+ * Java based configuration
+ * @author Geoffrey Duncan Opiyo
+ *
+ */
 public class HibernateUtil {
-    private static StandardServiceRegistry registry;
-    private static SessionFactory sessionFactory;  // Immutable - Encapsulation
+ private static SessionFactory sessionFactory;
 
-    public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-            	// All the 3 below classes uses BUILDER Pattern.
-                // Create registry
-                registry = new StandardServiceRegistryBuilder().configure().build();
+ public static SessionFactory getSessionFactory() {
+  if (sessionFactory == null) {
+   try {
+    Configuration configuration = new Configuration();
 
-                // Create MetadataSources
-                MetadataSources sources = new MetadataSources(registry);
+    // Hibernate settings equivalent to hibernate.cfg.xml's properties
+    Properties settings = new Properties();
+    settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+    settings.put(Environment.URL, "jdbc:mysql://localhost:3306/flyaway_db?useSSL=false");
+    settings.put(Environment.USER, "webstudent");
+    settings.put(Environment.PASS, "webstudent");
+    settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
 
-                // Create Metadata
-                Metadata metadata = sources.getMetadataBuilder().build();
+    settings.put(Environment.SHOW_SQL, "true");
 
-                // Create SessionFactory
-                sessionFactory = metadata.getSessionFactoryBuilder().build();
+    settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
-            } catch (Exception e) {
-                e.printStackTrace();
-                if (registry != null) {
-                    StandardServiceRegistryBuilder.destroy(registry);
-                }
-            }
-        }
-        return sessionFactory;
-    }
+    settings.put(Environment.HBM2DDL_AUTO, "create-drop");
 
-    public static void shutdown() {
-        if (registry != null) {
-            StandardServiceRegistryBuilder.destroy(registry);
-        }
-    }
+    configuration.setProperties(settings);
+    configuration.addAnnotatedClass(Flight.class);
+    configuration.addAnnotatedClass(Passengers.class);
+
+    ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+      .applySettings(configuration.getProperties()).build();
+    System.out.println("Hibernate Java Config serviceRegistry created");
+    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+    return sessionFactory;
+
+   } catch (Exception e) {
+    e.printStackTrace();
+   }
+  }
+  return sessionFactory;
+ }
 }
