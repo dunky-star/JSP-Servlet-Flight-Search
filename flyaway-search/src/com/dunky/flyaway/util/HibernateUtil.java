e5beeb4323783a293d0afb3d9ab645fd1a10ca20
dunky-star <dukygeoffrey39@gmail.com>
2022-10-05 13:52:22 +0300

    Move Hibernate xml config file to class path

--- flyaway-search/src/com/dunky/flyaway/util/HibernateUtil.java
+++ flyaway-search/src/com/dunky/flyaway/util/HibernateUtil.java
@@ -1,57 +1,44 @@
 package com.dunky.flyaway.util;
 
-import java.util.Properties;
-
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
-import org.hibernate.service.ServiceRegistry;
 
-import com.dunky.flyaway.entity.Flight;
-import com.dunky.flyaway.entity.Passengers;
-
-/**
- * Java based configuration
- * @author Geoffrey Duncan Opiyo
- *
- */
 public class HibernateUtil {
- private static SessionFactory sessionFactory;
+    private static StandardServiceRegistry registry;
+    private static SessionFactory sessionFactory;  // Immutable - Encapsulation
 
- public static SessionFactory getSessionFactory() {
-  if (sessionFactory == null) {
-   try {
-    Configuration configuration = new Configuration();
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+            	// All the 3 below classes uses BUILDER Pattern.
+                // Create registry
+                registry = new StandardServiceRegistryBuilder().configure().build();
 
-    // Hibernate settings equivalent to hibernate.cfg.xml's properties
-    Properties settings = new Properties();
-    settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-    settings.put(Environment.URL, "jdbc:mysql://localhost:3306/flyaway_db?useSSL=false");
-    settings.put(Environment.USER, "webstudent");
-    settings.put(Environment.PASS, "webstudent");
-    settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+                // Create MetadataSources
+                MetadataSources sources = new MetadataSources(registry);
 
-    settings.put(Environment.SHOW_SQL, "true");
+                // Create Metadata
+                Metadata metadata = sources.getMetadataBuilder().build();
 
-    settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                // Create SessionFactory
+                sessionFactory = metadata.getSessionFactoryBuilder().build();
 
-    settings.put(Environment.HBM2DDL_AUTO, "update");
+            } catch (Exception e) {
+                e.printStackTrace();
+                if (registry != null) {
+                    StandardServiceRegistryBuilder.destroy(registry);
+                }
+            }
+        }
+        return sessionFactory;
+    }
 
-    configuration.setProperties(settings);
-    configuration.addAnnotatedClass(Flight.class);
-    configuration.addAnnotatedClass(Passengers.class);
-
-    ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-      .applySettings(configuration.getProperties()).build();
-    System.out.println("Hibernate Java Config serviceRegistry created");
-    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-    return sessionFactory;
-
-   } catch (Exception e) {
-    e.printStackTrace();
-   }
-  }
-  return sessionFactory;
- }
+    public static void shutdown() {
+        if (registry != null) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
 }
